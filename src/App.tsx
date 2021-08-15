85b1f5df9089f9d2e5bb77daa738ac4eb21a9a64
Mason Miller <masonmiller11@gmail.com>
2021-08-15 18:52:22 -0400

    Basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import FavoritesPage from './pages/FavoritesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Test
-        </p>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route path='/auth'>
+          <AuthPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout/MainNavigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainNavigation() {
+  return (
+    <div >
+        <p>Nav</p>
+    </div>
+  );
+}
+
+export default MainNavigation;

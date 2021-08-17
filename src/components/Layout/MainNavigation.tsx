e4e20fa9dd4b19dd01e3056175b2b84ced12727a
Mason Miller <masonmiller11@gmail.com>
2021-08-16 20:55:30 -0400

    Basic style for navbar

--- src/components/Layout/MainNavigation.tsx
+++ src/components/Layout/MainNavigation.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Alignment, Button, Classes, H5, Navbar, NavbarDivider, NavbarGroup, NavbarHeading} from "@blueprintjs/core";
 
-function MainNavigation() {
-  return (
-    <div >
-        <p>Nav</p>
-    </div>
-  );
+const MainNavigation = () => {
+
+    let history = useHistory();
+
+    return (
+        <Navbar className = 'bp3-navbar bp3-dark'>
+            <NavbarGroup align={Alignment.LEFT}>
+                <NavbarHeading>NMA Tracker</NavbarHeading>
+                <NavbarDivider />
+                <Button onClick={() => history.push('/')} className={Classes.MINIMAL} icon="home" text="Home" />
+                <Button onClick={() => history.push('/favorites')} className={Classes.MINIMAL} icon="document" text="Favorites" />
+                <Button onClick={() => history.push('/auth')} className={Classes.MINIMAL} icon="document" text="Login"/>
+            </NavbarGroup>
+        </Navbar>
+    )
 }
 
 export default MainNavigation;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@blueprintjs/core/lib/css/blueprint.css";
 
 ReactDOM.render(
   <React.StrictMode>

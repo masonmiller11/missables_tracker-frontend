353aaac3ac51625420a8c0cbe63c90ef67abb1c1
Mason Miller <masonmiller11@gmail.com>
2021-08-24 21:30:53 -0400

    Add catch all route

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import { Classes } from "@blueprintjs/core";
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Classes } from '@blueprintjs/core';
 
-import Layout from "./components/Layout/Layout";
-import AuthPage from "./pages/AuthPage";
-import HomePage from "./pages/HomePage";
-import FavoritesPage from "./pages/FavoritesPage";
-import "./App.css";
+import Layout from './components/Layout/Layout';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import FavoritesPage from './pages/FavoritesPage';
+import './App.css';
 
 function App() {
-  return (
-    <div className="dark-content-wrapper">
-      <div className={Classes.DARK}>
-        <Layout>
-          <Switch>
-            <Route path="/" exact>
-              <HomePage />
-            </Route>
-            <Route path="/login">
-              <AuthPage />
-            </Route>
-            <Route path="/favorites">
-              <FavoritesPage />
-            </Route>
-          </Switch>
-        </Layout>
-      </div>
-    </div>
-  );
+    return (
+        <div className="dark-content-wrapper">
+            <div className={Classes.DARK}>
+                <Layout>
+                    <Switch>
+                        <Route path="/" exact>
+                            <HomePage />
+                        </Route>
+                        <Route path="/login">
+                            <AuthPage />
+                        </Route>
+                        <Route path="/favorites">
+                            <FavoritesPage />
+                        </Route>
+                        <Route path="*">
+                            <Redirect to="/" />
+                        </Route>
+                    </Switch>
+                </Layout>
+            </div>
+        </div>
+    );
 }
 
 export default App;

ef9d8ba292ba5d9bec18914d031ff42b221117e7
Mason Miller <masonmiller11@gmail.com>
2021-08-29 22:36:38 -0400

    Add <form> tags

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -16,11 +16,8 @@ import {
     Classes,
     FormGroup,
 } from '@blueprintjs/core';
-import { useHistory } from 'react-router-dom';
 
-import { apiLogin } from '../../api/index';
 import classes from './AuthForm.module.css';
-import AuthContext from '../../store/auth-context';
 
 type AuthRefs = {
     emailInputRef: React.RefObject<HTMLInputElement>;
@@ -54,78 +51,80 @@ const AuthForm: React.FC<{
 
     return (
         <div>
-            <FormGroup
-                className={classes.textField}
-                intent={intent}
-                helperText={loginError ? loginError : loginNotifcation}
-            >
-                <Label>
-                    Email Address
-                    <InputGroup
-                        className={classes.textField}
-                        placeholder="email"
-                        large={true}
-                        inputRef={refs.emailInputRef}
-                        intent={intent}
-                    />
-                </Label>
-                {!isLogin ? (
+            <form onSubmit={onSubmit}>
+                <FormGroup
+                    className={classes.textField}
+                    intent={intent}
+                    helperText={loginError ? loginError : loginNotifcation}
+                >
                     <Label>
-                        Username
+                        Email Address
                         <InputGroup
                             className={classes.textField}
-                            placeholder="username"
+                            placeholder="email"
                             large={true}
+                            inputRef={refs.emailInputRef}
                             intent={intent}
-                            inputRef={refs.usernameInputRef}
                         />
                     </Label>
-                ) : null}
-                <Label>
-                    Password
-                    <InputGroup
-                        className={classes.textField}
-                        placeholder="password"
-                        large={true}
-                        type="password"
-                        intent={intent}
-                        inputRef={refs.passwordInputRef}
-                    />
-                </Label>
-                {!isLogin ? (
+                    {!isLogin ? (
+                        <Label>
+                            Username
+                            <InputGroup
+                                className={classes.textField}
+                                placeholder="username"
+                                large={true}
+                                intent={intent}
+                                inputRef={refs.usernameInputRef}
+                            />
+                        </Label>
+                    ) : null}
                     <Label>
-                        Confirm Password
+                        Password
                         <InputGroup
                             className={classes.textField}
                             placeholder="password"
                             large={true}
                             type="password"
                             intent={intent}
-                            inputRef={refs.confirmPasswordInputRef}
+                            inputRef={refs.passwordInputRef}
                         />
                     </Label>
-                ) : null}
-                {isLoading ? (
-                    <Spinner size={30} />
-                ) : (
-                    <Button
-                        className={classes.login}
-                        text={isLogin ? 'Sign In' : 'Sign Up'}
-                        onClick={(event) => onSubmit(event)}
-                    />
-                )}
-                {isLogin ? (
-                    <p className={classes.login}>
-                        Not a member yet? Click{' '}
-                        <a onClick={onLoginChange}>here</a> to sign up!
-                    </p>
-                ) : (
-                    <p className={classes.login}>
-                        Already a member? Click{' '}
-                        <a onClick={onLoginChange}>here</a> to sign in!
-                    </p>
-                )}
-            </FormGroup>
+                    {!isLogin ? (
+                        <Label>
+                            Confirm Password
+                            <InputGroup
+                                className={classes.textField}
+                                placeholder="password"
+                                large={true}
+                                type="password"
+                                intent={intent}
+                                inputRef={refs.confirmPasswordInputRef}
+                            />
+                        </Label>
+                    ) : null}
+                    {isLoading ? (
+                        <Spinner size={30} />
+                    ) : (
+                        <Button
+                            className={classes.login}
+                            text={isLogin ? 'Sign In' : 'Sign Up'}
+                            type="submit"
+                        />
+                    )}
+                    {isLogin ? (
+                        <p className={classes.login}>
+                            Not a member yet? Click{' '}
+                            <a onClick={onLoginChange}>here</a> to sign up!
+                        </p>
+                    ) : (
+                        <p className={classes.login}>
+                            Already a member? Click{' '}
+                            <a onClick={onLoginChange}>here</a> to sign in!
+                        </p>
+                    )}
+                </FormGroup>
+            </form>
         </div>
     );
 };

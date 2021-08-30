fb0a616e3305a45c8e141c2e899b12b8b47adcc8
Mason Miller <masonmiller11@gmail.com>
2021-08-29 22:36:59 -0400

    Add comment

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,4 +1,7 @@
 export const endpoints = {
     authentication : 'login',
-    signup : 'signup'
+    signup : 'signup',
+    // listLeads: (itemsPerPage: number, page: number) => {
+    //     return 'leads/' + page + '/' + itemsPerPage;
+    // },
 }

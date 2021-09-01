37e7d027b8787fbda88862e937fe41de168bb909
Mason Miller <masonmiller11@gmail.com>
2021-08-31 20:48:28 -0400

    Create game data model stub

---
+++ src/models/game.ts
@@ -0,0 +1,39 @@
+class game {
+
+}
+
+export default game;
+
+// type emailAddress = {
+//     emailAddress: string;
+//     emailID: number;
+// };
+
+// type name = {
+//     firstName: string;
+//     lastName: string;
+//     nameID: number;
+// };
+
+// type phoneNumber = {
+//   phoneNumber: string;
+//   phoneNumberID: number;
+// }
+
+// type postalCode = {
+//   postalCode: string;
+//   postalCodeID: number;
+// }
+
+// type leadInfo = {
+//   names: name[];
+//   emailAddresses: emailAddress[];
+//   phoneNumbers: phoneNumber[];
+//   postalCodes: postalCode[];
+// }
+
+// export type lead = {
+//   leadID: number;
+//   createdAt: string;
+//   leadInfo: leadInfo;
+// }

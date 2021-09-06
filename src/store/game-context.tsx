693afb54600a60ebaca21f6b473e38e49d4035f3
Mason Miller <masonmiller11@gmail.com>
2021-09-06 14:13:13 -0400

    Create GameContext

---
+++ src/store/game-context.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+const GameContext = null;
+
+export const GameContextProvider: React.FC = (props) => {
+  return (
+    <div>
+      <header>
+        <p>Favorites</p>
+      </header>
+    </div>
+  );
+}
+
+export default GameContext;

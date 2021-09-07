35e0df7796d71d7afa8bf33c4e0c63577a74fd30
Mason Miller <masonmiller11@gmail.com>
2021-09-06 21:06:52 -0400

    Add search async functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { CancelToken, CancelTokenSource } from 'axios';
 
 import { endpoints } from './endpoints';
 
@@ -15,26 +15,54 @@ export async function apiLogin(email: string, password: string) {
     return response;
 }
 
-export async function apiSignUp(email: string, password: string, username: string) {
+export async function apiSignUp(
+    email: string,
+    password: string,
+    username: string
+) {
     const response = await client.post(endpoints.signup, {
         username: username,
         password: password,
-        email: email
+        email: email,
     });
 
     return response;
 }
 
 export async function apiListPopularGames(
-    itemsPerPage: number|null = null,
-    page: number|null = null,
-    token: string|null =null
+    itemsPerPage: number | null = null,
+    page: number | null = null,
+    source: CancelTokenSource,
+    token: string | null = null
 ) {
-
     const endpoint = endpoints.listPopularGames(itemsPerPage, page);
+
+    const response = await client.get(endpoint,{cancelToken: source.token});
+
+    return response;
+}
+
+export async function apiSearch(searchTerm: string) {
+    const searchIGDBRequest = client.get(endpoints.searchIGDB(searchTerm));
+    const searchGamesRequest = client.get(endpoints.searchGames(searchTerm));
+
+    const response = await axios.all([searchIGDBRequest, searchGamesRequest]);
+
+    return response;
+}
+
+export async function apiSearchIGDB(searchTerm: string) {
+    const endpoint = endpoints.searchIGDB(searchTerm);
 
     const response = await client.get(endpoint);
 
     return response;
+}
 
+export async function apiSearchGames(searchTerm: string) {
+    const endpoint = endpoints.searchGames(searchTerm);
+
+    const response = await client.get(endpoint);
+
+    return response;
 }

68d3c629f86ab4e9aa1b53375109b9683a553547
Mason Miller <masonmiller11@gmail.com>
2021-10-21 21:03:35 -0400

    Create Template button and button handlers added

--- src/components/Layout/GameList/GamesCard/GamesCard.tsx
+++ src/components/Layout/GameList/GamesCard/GamesCard.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import {
-    Button,
-    Card,
-    Classes,
-    Elevation,
-    H5
-} from '@blueprintjs/core';
+import { useHistory } from 'react-router-dom';
+import { Button, Card, Classes, Elevation, H5 } from '@blueprintjs/core';
 
 import Game from '../../../../api/models/Game/Game';
 import classes from './GamesCard.module.css';
 
 const GamesCard: React.FC<{ game: Game }> = ({ game }) => {
+    
+    let history = useHistory();
+
+    const createButton = (
+        onClick: (gameId: number) => void,
+        gameId: number,
+        buttonText: string
+    ): React.HTMLProps<HTMLButtonElement> => {
+        const onClickHandler = () => onClick(gameId);
+
+        return (
+            <Button
+                text={buttonText}
+                onClick={onClickHandler}
+                className={Classes.BUTTON}
+            />
+        );
+    };
+
+    const onSeeChecklists = (gameId: number) => {
+        history.replace('/guides/' + gameId);
+    };
+
+    const onCreateTemplate = (gameId: number) => {
+        history.replace('/guides/create' + gameId);
+    };
+
     return (
         <Card
             className={classes.gamesCard}
@@ -27,7 +49,9 @@ const GamesCard: React.FC<{ game: Game }> = ({ game }) => {
                 <p>Total Playthroughs: {game.playthroughCount}</p>
                 <p>Total Templates: {game.templateCount}</p>
 
-                <Button text="See Checklists" className={Classes.BUTTON} />
+                {game.templateCount > 0
+                    ? createButton(onSeeChecklists, game.id, 'See Templates')
+                    : createButton(onCreateTemplate, game.id, 'Create Guide')}
             </div>
         </Card>
     );

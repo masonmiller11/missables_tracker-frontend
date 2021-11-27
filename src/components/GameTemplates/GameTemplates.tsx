d98dcb8e506ea392a519be5546d9f1888e5fe5b1
Mason Miller <masonmiller11@gmail.com>
2021-11-26 22:08:53 -0500

    Change folder name

--- src/components/GameTemplates/GameTemplates.tsx
+++ src/components/GameTemplates/GameTemplates.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import Templatelist from '../Templates/TemplateList';
+import Templatelist from '../TemplateList/TemplateList';
 import GameSummaryCard from '../Layout/GameSummary/GameSummaryCard';
 import classes from './GameTemplates.module.css';
 

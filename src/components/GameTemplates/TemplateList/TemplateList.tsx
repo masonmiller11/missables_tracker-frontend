c59e8bec0901d4648f6a9e8e389620b783325e1e
Mason Miller <masonmiller11@gmail.com>
2021-12-26 13:33:20 -0500

    Remove fake payload

---
+++ src/components/GameTemplates/TemplateList/TemplateList.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Spinner } from '@blueprintjs/core';
+
+import { apiListTemplates } from '../../../api';
+import Template from '../../../api/models/Template/Template';
+import classes from './TemplateList.module.css';
+import TemplateCard from './TemplateCard/TemplateCard';
+
+const TemplateList: React.FC<{ gameId: string }> = ({ gameId: gameIdProp }) => {
+
+    const [templateList, setTemplateList] = useState<null | Template[]>(null);
+
+    useEffect(() => {
+        
+        let source = axios.CancelToken.source();
+
+        apiListTemplates(gameIdProp, source)
+            .then((response) => {
+                setTemplateList(response.data.templates);
+            })
+            .catch((err) => {
+                if (axios.isCancel(err)) {
+                    console.log('api request cancelled');
+                } else {
+                    console.log(err.response?.data.message ?? 'unknown error');
+                }
+            });
+
+        //todo add real error handling
+ 
+    }, []);
+
+
+    if (templateList) {
+        return (
+            <div className={classes.templateListContainer}>
+                {/* <p>Game Id To Pull Templates: {gameIdProp}</p> */}
+                {templateList!.map((template) => (
+                    <TemplateCard template={template} />
+                ))}
+            </div>
+        );
+    }
+    {
+        return <div></div>;
+    }
+};
+
+export default TemplateList;

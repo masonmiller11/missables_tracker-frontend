5d5dff20615f56f75f60592b8b4321e30a6c90a0
Mason Miller <masonmiller11@gmail.com>
2022-01-03 18:59:30 -0500

    Clean up functionality

--- src/components/Button/AddNewButton/AddNewButton.tsx
+++ src/components/Button/AddNewButton/AddNewButton.tsx
@@ -7,43 +7,48 @@ import {
 	Classes,
 	Intent,
 	Callout,
-	Icon
+	Icon,
+	SpinnerSize
 } from '@blueprintjs/core';
 
 import classes from './AddNewButton.module.css';
+import { SMALL } from "@blueprintjs/core/lib/esm/common/classes";
 
-const EditButton: React.FC<{
-	displaySaveFirstMessage: boolean
+const AddNewButton: React.FC<{
 	onClick: (() => void)
 	savingNewObject: boolean
+	objectName: string
 }> = ({
-	displaySaveFirstMessage,
 	onClick,
-	savingNewObject
+	savingNewObject,
+	objectName
 }) => {
 
 		if (savingNewObject)
-			return <Spinner />
+			return <Spinner
+				size={SpinnerSize.SMALL}
+				className={classes.newButton}
+			/>
 
-		if (!displaySaveFirstMessage)
+
 			return <Button
 				icon="add"
 				className={classes.newButton}
 				onClick={onClick}
-				text="New Step"
+				text={"New " + objectName}
 			/>
 
-		return <Callout
-			intent={Intent.WARNING} 
-			className={classes.saveFirstCallout}
-			// title="You Must Save First"
-			>
-				You must save this section before adding another step. Click the save button in the top right of the section card.
-				If you do not save, your new step will be lost.
-			</Callout>
+		// return <Callout
+		// 	intent={Intent.WARNING}
+		// 	className={classes.saveFirstCallout}
+		// // title="You Must Save First"
+		// >
+		// 	You must save this section before adding another step. Click the save button in the top right of the section card.
+		// 	If you do not save, your new step will be lost.
+		// </Callout>
 
 		//Callout Click save to keep new {object}. Add Delete New {object} button to the right.
 
 	};
 
-export default EditButton;
+export default AddNewButton;

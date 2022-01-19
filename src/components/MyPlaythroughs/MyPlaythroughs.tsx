176cff87bb6dc0a72b853b977699033239b1e1e5
Mason Miller <masonmiller11@gmail.com>
2022-01-18 20:42:18 -0500

    Create MyPlaythroughs

--- src/components/MyPlaythroughs/MyPlaythroughs.tsx
+++ src/components/MyPlaythroughs/MyPlaythroughs.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { Spinner } from '@blueprintjs/core';
+import axios from 'axios';
+
+import PlaythroughModel from '../../api/models/Playthrough/Playthrough';
+import ResponseDataModel from '../../api/models/ResponseData';
+import PlaythroughList from '../GamePlaythroughs/PlaythroughList/PlaythroughList';
+import useApi from '../../hooks/useApi';
+import { apiDeletePlaythrough, apiListThisUsersPlaythroughs } from '../../api';
+import AuthContext from '../../store/auth-context';
+
+import classes from './MyPlaythroughs.module.css';
 
 const MyPlaythroughs: React.FC = () => {
 
+	const [playthroughList, setPlaythroughList] = useState<PlaythroughModel[] | null>(null);
+	const { apiGetRequestWithToken: apiGetRequest, apiDeleteRequest } = useApi();
+	const AuthCtx = useContext(AuthContext);
+
+	const applyPlaythroughResponseData = (responseData: ResponseDataModel<PlaythroughModel>) => {
+		setPlaythroughList(responseData.items)
+	}
+
+	useEffect(() => {
+
+		let source = axios.CancelToken.source();
+
+		if (AuthCtx.token)
+			apiGetRequest<PlaythroughModel[] | null>(applyPlaythroughResponseData, apiListThisUsersPlaythroughs, AuthCtx.token, source);
+
+		return function () {
+			source.cancel('cancelling in cleanup');
+		}
+
+	}, [AuthCtx]);
+
+	const deletePlaythroughHandler = (playthroughToDelete: PlaythroughModel) => {
+
+		let source = axios.CancelToken.source();
+
+		if (AuthCtx.token)
+			apiDeleteRequest(playthroughToDelete.id, apiDeletePlaythrough, AuthCtx.token, source);
+
+		let newPlaythroughArray = playthroughList!.filter((playthrough) => {
+			return playthrough.id != playthroughToDelete.id
+		});
+
+		setPlaythroughList(newPlaythroughArray);
+
+	}
+
+	const playthroughListOptions = {
+		onDelete: deletePlaythroughHandler,
+		playthroughUrl: '/myplaythroughs/'
+	}
+
+	if (playthroughList) {
+		console.log('playthroughList:' + playthroughList)
+		return (
+			<div className={classes.myPlaythroughsBackground}>
+				<div className={classes.myPlaythroughsContainer}>
+					<PlaythroughList
+						playthroughs={playthroughList}
+						playthroughListOptions={playthroughListOptions}
+					/>
+				</div>
+			</div>
+		);
+	}
+
 	return (
-		<p>My playthroughs</p>
+		<div className={classes.myPlaythroughsBackground}>
+			<div className={classes.myPlaythroughsContainer}>
+				<Spinner className={classes.spinner} />
+			</div>
+		</div>
 	);
 }
 

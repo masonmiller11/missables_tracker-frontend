40383aa82ee833c2b928ac3521cb00c40478e37f
Mason Miller <masonmiller11@gmail.com>
2022-01-18 20:31:05 -0500

    Remove TemplateStepLayout

--- src/components/Template/TemplateSection/TemplateStep/TemplateStepComponent.tsx
+++ src/components/Template/TemplateSection/TemplateStep/TemplateStepComponent.tsx
@@ -1,60 +1,98 @@
 import React, { useEffect } from "react";
+import { Icon, EditableText } from '@blueprintjs/core';
 
 import TemplateStepModel from '../../../../api/models/Template/TemplateStep';
-import TemplateStepLayout from './TemplateStepLayout/TemplateStepLayout';
 import useTemplateObject from '../../../../hooks/useTemplateObject';
+import DeleteButton from '../../../Button/DeleteButton/DeleteButton';
+
+import classes from './TemplateStep.module.css';
+
 
 const TemplateStep: React.FC<{
 	templateStep: TemplateStepModel,
 	onUpdateStep: (step: TemplateStepModel) => void,
 	onDeleteStep: (step: TemplateStepModel) => void,
 	editing: boolean,
-}> = ({
-	templateStep,
-	onUpdateStep,
-	onDeleteStep,
-	editing
-}) => {
+}> = ({ templateStep, onUpdateStep, onDeleteStep, editing }) => {
 
-		const {
-			object: step,
-			editObjectHandler: editStepHandler,
-			setObjectHandler: setStep
-		} = useTemplateObject<TemplateStepModel>(templateStep);
+	const {
+		object: step,
+		editObjectHandler: editStepHandler,
+		setObjectHandler: setStep
+	} = useTemplateObject<TemplateStepModel>(templateStep);
 
-		useEffect(() => {
-			setStep(templateStep);
-		}, [templateStep]);
+	useEffect(() => {
+		setStep(templateStep);
+	}, [templateStep]);
 
-		const changeStepPositionHandler = (event: string) => {
+	const changeStepPositionHandler = (event: string) => {
 
-			if (event === "") {
+		if (event === "") {
 
-				editStepHandler({ ...step, position: "" });
+			editStepHandler({ ...step, position: "" });
 
-			} else {
+		} else {
 
-				let positionNumber = (parseInt(event)) ?? setStep(step);
+			let positionNumber = (parseInt(event)) ?? setStep(step);
 
-				editStepHandler({ ...step, position: positionNumber });
+			editStepHandler({ ...step, position: positionNumber });
 
-			}
 		}
-
-		const saveStepHandler = () => {
-			onUpdateStep(step);
-		}
-
-		return (
-			<TemplateStepLayout
-				onChangeStepPosition={changeStepPositionHandler}
-				onSaveStep={saveStepHandler}
-				onDeleteStep={onDeleteStep}
-				onEditStep={editStepHandler}
-				editing={editing}
-				step={step}
-			/>
-		);
 	}
+
+	const saveStepHandler = () => {
+		onUpdateStep(step);
+	}
+
+	return (
+		<div className={classes.stepContainer}>
+			<div className={classes.postionNameDeleteContainer}>
+				<div>
+					<Icon icon="tick" className={classes.tick} />
+					<div className={classes.positionAndNameContainer}>
+
+						<p><strong>Step #</strong> </p>
+						<div className={classes.position}>
+							<p><strong><EditableText
+								onChange={newValueString => {
+									changeStepPositionHandler(newValueString);
+								}}
+								disabled={!editing}
+								value={step.position.toString()}
+								maxLength={2}
+								onConfirm={() => saveStepHandler()}
+							/></strong></p>
+						</div>
+
+						<strong><EditableText
+							onChange={newValueString => {
+								editStepHandler({ ...step, name: newValueString })
+							}}
+							disabled={!editing}
+							value={step.name}
+							onConfirm={() => saveStepHandler()}
+						/></strong>
+
+					</div>
+				</div>
+
+				{editing && <DeleteButton danger={false} onDelete={() => onDeleteStep(step)} />}
+
+			</div>
+			<p className={classes.stepDescription}>
+
+				<EditableText
+					onChange={newValueString => {
+						editStepHandler({ ...step, description: newValueString })
+					}}
+					disabled={!editing}
+					value={step.description}
+					onConfirm={() => saveStepHandler()}
+					multiline={true}
+
+				/></p>
+		</div>
+	);
+}
 
 export default TemplateStep;

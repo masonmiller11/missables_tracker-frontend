3e34dfeb7654ebf6fe156a08ae528950a1c37ade
Mason Miller <masonmiller11@gmail.com>
2022-01-29 14:22:38 -0500

    Change countOfTotalItems type to number

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -2,10 +2,10 @@
 import { useState, Dispatch, SetStateAction } from 'react';
 
 interface ReturnedData {
-	countOfTotalItems: string | number,
+	countOfTotalItems: number,
 	pageNumber: number,
 	pageSize: number,
-	setCountOfTotalItems: Dispatch<SetStateAction<string | number>>,
+	setCountOfTotalItems: Dispatch<SetStateAction<number>>,
 	setPageSize: Dispatch<SetStateAction<number>>,
 	pageChangeHandler: (page: number) => void
 }
@@ -14,7 +14,7 @@ const usePagination = (initialPageNumber: number,
 	initialPageSize: number,
 	initialCountOfTotalItems: number = 0): ReturnedData => {
 
-	const [countOfTotalItems, setCountOfTotalItems] = useState<number | string>(initialCountOfTotalItems);
+	const [countOfTotalItems, setCountOfTotalItems] = useState<number>(initialCountOfTotalItems);
 	const [pageNumber, setPageNumber] = useState<number>(initialPageNumber);
 	const [pageSize, setPageSize] = useState<number>(initialPageSize);
 

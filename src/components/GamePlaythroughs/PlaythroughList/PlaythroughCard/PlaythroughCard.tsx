517d9c42b4e521b53a2e9ac25972f1b11cfb2a7d
Mason Miller <masonmiller11@gmail.com>
2022-01-30 15:05:27 -0500

    Add collapse functionality

--- src/components/GamePlaythroughs/PlaythroughList/PlaythroughCard/PlaythroughCard.tsx
+++ src/components/GamePlaythroughs/PlaythroughList/PlaythroughCard/PlaythroughCard.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Card } from '@blueprintjs/core';
+import { Card, Collapse, Button } from '@blueprintjs/core';
 
 import { Playthrough } from '../../../../api/models/Playthrough/Playthrough';
 import PlaythroughListOptions from '../../../../interfaces/PlaythroughListOptions.interface';
@@ -10,30 +10,40 @@ import classes from './PlaythroughCard.module.css';
 const PlaythroughCard: React.FC<{ playthrough: Playthrough, playthroughCardOptions: PlaythroughListOptions }> = ({ playthrough, playthroughCardOptions }) => {
 
 	let { onDelete, playthroughUrl } = playthroughCardOptions;
+	const [collapsed, setCollapsed] = useState<boolean>(true);
 
 	let history = useHistory();
 
 	return (
 		<Card className={classes.playthroughCard}>
 			<div className={classes.playthroughCardContainer}>
-
-				<img className={classes.cover} src={playthrough.game.cover}></img>
-
-				<div className={classes.playthroughCardTitleAndAuthorContainer}>
-
-					<div className={classes.playthroughCardTitleAndDeleteContainer}>
-						<h2>
-							<a onClick={() => history.push(playthroughUrl + playthrough.id)}>{playthrough.title}</a>
-						</h2>
-
-						<DeleteButton onDelete={() => onDelete(playthrough)} danger={false}
-						/>
+				<div className={classes.titleAndShowInfoButtonContainer}>
+					<h3 className={classes.playthroughName}>
+						<a onClick={() => history.push(playthroughUrl + playthrough.id)}>{playthrough.title} </a>
+						({playthrough.game.gameTitle} Playthrough)
+					</h3>
+					<div className={classes.buttonsContainer}>
+					<DeleteButton onDelete={() => onDelete(playthrough)} />
+					<Button
+						icon={collapsed ? "arrow-down" : "arrow-up"}
+						onClick={() => setCollapsed(!collapsed)}
+						text={collapsed ? "Show Details" : "Hide Details"}
+					/>
 					</div>
-					<p>Template Created by {playthrough.owner.owner}</p>
-					<p>{playthrough.description}</p>
 				</div>
+				<Collapse isOpen={!collapsed} keepChildrenMounted={true}>
+					<hr />
+					<div className={classes.imageAndInfoContainer}>
+						<img className={classes.cover} src={playthrough.game.cover}></img>
+
+						<div className={classes.playthroughDetailsContainer}>
+								<p>Template Created by {playthrough.owner.owner}</p>
+							<p>{playthrough.description}</p>
+						</div>
+					</div>
+				</Collapse>
 			</div>
-		</Card>
+		</Card >
 	);
 };
 

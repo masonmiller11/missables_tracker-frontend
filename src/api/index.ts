501339f6044f7dc6c9ac584d87337f0f4e2390a3
Mason Miller <masonmiller11@gmail.com>
2022-01-30 14:59:02 -0500

    Add token to header on requests & add apiRefresh method

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,8 +4,16 @@ export const client = axios.create({
 	baseURL: 'http://localhost:8000/',
 });
 
+client.interceptors.request.use( config => {
+
+	const token = localStorage.getItem('token')
+	config.headers['Authorization'] = 'Bearer ' + token;
+	return config; 
+})
+
+
 export function getConfig(token: string, source: CancelTokenSource) {
-	let config = {
+	const config = {
 		headers: {
 			Authorization: 'Bearer ' + token,
 		},
@@ -14,6 +22,7 @@ export function getConfig(token: string, source: CancelTokenSource) {
 
 	return config;
 }
+
 
 export async function apiLogin(email: string, password: string) {
 	const response = await client.post('login', {
@@ -37,3 +46,9 @@ export async function apiSignUp(
 
 	return response;
 }
+
+export async function apiRefresh() {
+	const response = await client.get('login/refresh');
+	return response;
+}
+

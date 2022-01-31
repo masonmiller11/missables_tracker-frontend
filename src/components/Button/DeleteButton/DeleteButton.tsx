f7742403fe5dffe150152e6c284392158acffbb0
Mason Miller <masonmiller11@gmail.com>
2022-01-30 19:00:49 -0500

    Add ability to change delete button text

--- src/components/Button/DeleteButton/DeleteButton.tsx
+++ src/components/Button/DeleteButton/DeleteButton.tsx
@@ -3,17 +3,27 @@ import { Button, Intent } from '@blueprintjs/core';
 
 import classes from './DeleteButton.module.css';
 
-const DeleteButton: React.FC<{ onDelete: (() => void) }> = ({
-	onDelete
+type props = {
+	onDelete: (() => void),
+	buttonText?: string
+}
+
+const DeleteButton: React.FC<props> = ({
+	onDelete,
+	buttonText
 }) => {
 
-	const [confirmDelete, setConfirmDelete] = useState<boolean>(false);
+	let deleteButtonText = "Delete"
 
+	if (buttonText) deleteButtonText = buttonText;
+
+	const [confirmDelete, setConfirmDelete] = useState<boolean>(false);
+	
 	return <div className={classes.deleteButtonContainer}>
 		<Button
 			icon={confirmDelete ? "trash" : "delete"}
 			onClick={!confirmDelete ? () => setConfirmDelete(true) : () => onDelete()}
-			text={confirmDelete ? "Confirm" : "Delete"}
+			text={confirmDelete ? "Confirm" : deleteButtonText}
 			intent={confirmDelete ? Intent.DANGER : Intent.NONE}
 			className={classes.button}
 		/>

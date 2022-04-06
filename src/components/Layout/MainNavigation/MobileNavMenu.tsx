aa1dde37f7367fb5469dc4458ba816511ae0f715
Mason Miller <masonmiller11@gmail.com>
2022-04-06 19:08:27 -0400

    Add mobile friendly main menu

--- src/components/Layout/MainNavigation/MobileNavMenu.tsx
+++ src/components/Layout/MainNavigation/MobileNavMenu.tsx
@@ -5,29 +5,37 @@ import { useHistory } from 'react-router-dom';
 
 import User from '../../../api/models/User';
 
-const MobileNavMenu: React.FC<{ isLoggedIn: boolean, user: User|null, onLogout: () => void }> = ({
-	isLoggedIn, 
+const MobileNavMenu: React.FC<{ isLoggedIn: boolean, user: User | null, onLogout: () => void }> = ({
+	isLoggedIn,
 	onLogout,
 	user
 }) => {
 
 	let history = useHistory();
-	
+
 	console.log('user:' + user?.username);
 
-    const menu = (
-        <Menu>
-            {/* <MenuItem icon="graph" text="Preferences" onClick={() => history.push('/preferences')}/> */}
-            {/* <MenuDivider /> */}
-            <MenuItem icon="log-out" text="Log Out" onClick={onLogout} />
-        </Menu>
-    );
+	const menu = (
+		<Menu large>
+			<MenuItem icon="home" text="Home" onClick={() => history.push('/')} />
+			{isLoggedIn ?
+				<React.Fragment>
+					<MenuItem icon="star" text="Favorites" onClick={() => history.push('/favorites')} />
+					<MenuItem icon="manual" text="My Guides" onClick={() => history.push('/myguides')} />
+					<MenuItem icon="send-to-map" text="My Playthroughs" onClick={() => history.push('/myplaythroughs')} />
+					<MenuItem icon="log-out" text="Log Out" onClick={onLogout} />
+				</React.Fragment>
+				:
+				<MenuItem icon="log-in" text="Log In" onClick={() => history.push('/login')} />
+			}
+		</Menu>
+	);
 
-    return (
-        <Popover content={menu}>
-            <Button icon="menu"/>
-        </Popover>
-    );
+	return (
+		<Popover content={menu}>
+			<Button icon="menu" />
+		</Popover>
+	);
 };
 
 export default MobileNavMenu;
